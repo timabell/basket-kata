8279ed649508ea75210f565977e9444769000795
Tim Abell <tim@timwise.co.uk>
2022-01-12 14:52:24 +0000

    Blank spec project
    generated with
    `dotnet new nunit --name specs`

---
+++ specs/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace specs;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
