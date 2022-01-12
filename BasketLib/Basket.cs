67d51105d0da8b89f4506513f8e1ba494f320709
Tim Abell <tim@timwise.co.uk>
2022-01-12 17:25:03 +0000

    Calculate and record discount in basket
    TestDiscountedTotal now passes

--- BasketLib/Basket.cs
+++ BasketLib/Basket.cs
@@ -6,6 +6,7 @@ public class Basket
 	// todo: Add a discount method, pass in a IDiscounter that takes actions with some kind of callback (to avoid tight coupling or embedding the discount code in the basket)
 	// todo: Make a discounter that walks the basket list applies the discounts - danger, mutation, maybe yield a discount
 	private readonly List<Product> _products = new();
+	private readonly List<Discount> _discounts = new();
 
 	public void Add(Product product)
 	{
@@ -14,11 +15,13 @@ public class Basket
 
 	public decimal Total()
 	{
-		return _products.Sum(p => p.Price);
+		var subtotal = _products.Sum(p => p.Price);
+		var discountSubtotal = _discounts.Sum(d => d.Amount);
+		return subtotal - discountSubtotal;
 	}
 
 	public void ApplyDiscounts(IDiscounter discounter)
 	{
-		//todo: discounter.Calculate(_products);
+		_discounts.AddRange(discounter.Calculate(_products));
 	}
 }
