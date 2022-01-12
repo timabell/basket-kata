eaabc6f2a569e91ef11c3cb0f48e4b041fdc594d
Tim Abell <tim@timwise.co.uk>
2022-01-12 16:16:39 +0000

    Add some todos designing structure for applying discounts
    Note that I wouldn't have these TODOs make it as far as a pull request.
    In fact they'd probably get edited out of branch history before opening
    a PR, or I'd normally make notes like this out of band.
    
    http://wordaligned.org/articles/todo

--- specs/Basket.cs
+++ specs/Basket.cs
@@ -5,7 +5,11 @@ namespace specs;
 
 internal class Basket
 {
+	// todo: Convert products to list of basket items with product + discount so we have somewhere to store discounts, update total calcs to include them
+	// todo: Add a discount method, pass in a IDiscounter that takes actions with some kind of callback (to avoid tight coupling or embedding the discount code in the basket)
+	// todo: Make a discounter that walks the basket list applies the discounts - danger, mutation, maybe yield a discount
 	private List<Product> _products = new();
+
 	public void Add(Product product)
 	{
 		_products.Add(product);
