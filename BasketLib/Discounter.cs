7dccdf78cccb9e7ae8ed4ec1b038ac6280d7ec3c
Tim Abell <tim@timwise.co.uk>
2022-01-12 17:50:33 +0000

    Implement and test bread discount

--- BasketLib/Discounter.cs
+++ BasketLib/Discounter.cs
@@ -11,13 +11,42 @@ public class Discounter : IDiscounter
 	public IEnumerable<Discount> Calculate(List<Product> products)
 	{
 		var discounts = new List<Discount>();
+
+		// we could potentially go polymorphic with this, but that ties us further in to pure code rather than
+		// data-driven discount systems which will be harder to iterate fast on or let non-coders modify.
+		discounts.AddRange(MilkDiscount(products));
+		discounts.AddRange(BreadDiscount(products));
+
+		return discounts;
+	}
+
+	private IEnumerable<Discount> BreadDiscount(List<Product> products)
+	{
 		// todo: replace string matching with some better referencing system
-		// 4th milk free (todo: validate with BA whether this also applies to 8th milk, currently no)
+		var butters = products.Where(p => p.Name.Equals("butter", StringComparison.InvariantCultureIgnoreCase));
+		var bread = products.FirstOrDefault(p => p.Name.Equals("bread", StringComparison.InvariantCultureIgnoreCase));
+
+		// half price bread with 2x butter (todo: validate if this is one offer per customer or if they can stack em up)
+		if ((butters.Count() >= 2) && bread != null)
+		{
+			return new List<Discount> { new() { Amount = bread.Price * 0.5m, Description = "Half price bread with 2x butter" } };
+		}
+
+		return new List<Discount>();
+	}
+
+	private static IEnumerable<Discount> MilkDiscount(IEnumerable<Product> products)
+	{
+		// todo: replace string matching with some better referencing system
 		var milks = products.Where(p => p.Name.Equals("milk", StringComparison.InvariantCultureIgnoreCase));
+
+		// 4th milk free (todo: validate with BA whether this also applies to 8th milk, currently no)
 		if (milks.Count() > 3)
 		{
-			discounts.Add(new Discount{Amount = milks.First().Price, Description = "Free 4th milk"});
+			return new List<Discount> { new() { Amount = milks.First().Price, Description = "Free 4th milk" } };
 		}
-		return discounts;
+
+		return new List<Discount>();
+
 	}
 }
--- specs/DiscounterTests.cs
+++ specs/DiscounterTests.cs
@@ -23,4 +23,18 @@ public class DiscounterTests
 		discounts.Single().Amount.Should().Be(1.15m);
 		discounts.Single().Description.Should().Be("Free 4th milk");
 	}
+
+	[Test]
+	public void AppliesBreadDiscount()
+	{
+		var discounter = new Discounter();
+		var basketItems = new List<Product>();
+		var butter = new Product("butter", 0.8m);
+		2.Times(()=> basketItems.Add(butter));
+		basketItems.Add(new Product("bread", 1m));
+		var discounts = discounter.Calculate(basketItems);
+		discounts.Should().ContainSingle();
+		discounts.Single().Amount.Should().Be(new Product("bread", 1m).Price * 0.5m); // 50% off bread
+		discounts.Single().Description.Should().Be("Half price bread with 2x butter");
+	}
 }
