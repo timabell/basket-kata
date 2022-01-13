13c5c02dd1b0bb4c16b37c47f4db40d9ee62fd38
Tim Abell <tim@timwise.co.uk>
2022-01-13 17:46:11 +0000

    Sketch out basket form

--- react/basket/src/App.js
+++ react/basket/src/App.js
@@ -5,10 +5,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="logo.svg" className="App-logo" alt="logo" />
-        <p>
+        <h1>
 			<a href="https://github.com/timabell/basket-kata" target="_blank">Basket Kata</a>, now with added ReactJs
-        </p>
+        </h1>
       </header>
+	<body>
+		<div id="basket">
+			<h2>Add items</h2>
+			<div>
+				<label>Name: <input placeholder={"e.g. Milk"}></input></label>
+				<br/>
+				<label>Price: <input placeholder={"£0.00"}></input></label>
+				<br/>
+				<button>Add</button>
+			</div>
+			<h2>Basket</h2>
+			<ol>
+				<li>basket things here</li>
+				<li>...</li>
+			</ol>
+			<h2>Empty basket</h2>
+			<div>
+				<button>Clear</button>
+			</div>
+		</div>
+	</body>
     </div>
   );
 }
