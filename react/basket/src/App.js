9af4b13a25f3f7e6b731227fffa525ee6cc726ac
Tim Abell <tim@timwise.co.uk>
2022-01-13 17:38:15 +0000

    Make the front page my own
    It's the charm consulting logo now.

--- react/basket/src/App.js
+++ react/basket/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="logo.svg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+			<a href="https://github.com/timabell/basket-kata" target="_blank">Basket Kata</a>, now with added ReactJs
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
