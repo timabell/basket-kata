595ad601c91f397d912b460c2995c6d64148aac8
Tim Abell <tim@timwise.co.uk>
2022-01-13 10:41:08 +0000

    Update code comments

--- BasketLib/Discounter.cs
+++ BasketLib/Discounter.cs
@@ -1,8 +1,8 @@
 namespace BasketLib;
 
 /// <summary>
-/// A stateful visitor (pattern) that walks a basket of goods calculating discounts and then returning what to apply
-/// with object references.
+/// A stateful visitor (pattern) that walks a basket of goods calculating discounts and then returning what to apply.
+/// We could in future add object references so we know which item got the discount.
 /// Note that the logic for real discounting is non-trivial so I won't be implementing a bullet-proof version of this,
 /// just enough to pass the tests for now as a basis for iterating. I had a friend who worked on a real EPOS system as it happens.
 /// </summary>
